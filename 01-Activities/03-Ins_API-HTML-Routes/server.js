bfe06f66c1b4754303d5f9c2b11969d1a9c81379
Ishan <ishanwijes@gmail.com>
2022-07-26 20:03:02 +1000

    student activity 4 completed - get requests for json objects

--- 01-Activities/03-Ins_API-HTML-Routes/server.js
+++ 01-Activities/03-Ins_API-HTML-Routes/server.js
@@ -3,19 +3,30 @@ const express = require('express');
 const PORT = 3001;
 const app = express();
 
+
+
 app.get('/', (req, res) => {
+  // can send back HTML by using .send()
+  // can write as much HTML in .send() as you like
+  // what it actually sends back is a html file with nothing in the head, and only what you write in .send() in the body
   res.send(
-    `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
+    `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>
+    <p>This is another p tag</p>`
   );
 });
 
+
+
 app.get('/api', (req, res) => {
+  // this sends back a json object
   res.json({
     term: 'api',
     description:
       'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
   });
 });
+
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
--- 01-Activities/04-Stu_API-HTML-Routes/Unsolved/server.js
+++ 01-Activities/04-Stu_API-HTML-Routes/Unsolved/server.js
@@ -1,12 +1,20 @@
-// TODO: Import express
+// Import express
+const express = require('express')
+const path = require('module')
 
-// TODO: Import 'terms.json' file
+// Import 'terms.json' file
+const termsJSON = require('./terms.json')
 const PORT = 3001;
 
-// TODO: Initialize app variable
+// Initialize app variable
+const app = express()
 
-// TODO: Create a route for a GET request that will return the content of our `terms.json` file
+// Create a route for a GET request that will return the content of our `terms.json` file
+app.get('/terms', (req, res) => {
+  res.json(termsJSON);
+})
 
+// listen to requests on this port
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
